3c063bde943328258e408b63c0604a2167128f6c
Yeongmin Kim <oinochoe@naver.com>
2020-02-20 20:17:22 +0900

    inline-css module 뭉치

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express');
 const fs = require('fs');
-const http = require('http');
-const app = express();
 const inlineCss = require('inline-css');
 var html = "";
 
-fs.readFile('./origin/origin.html', 'utf8', (error, data) => {
-  if(error) throw error;
-  console.log("success");
-  html = data;
-});
-
-// writeFile
-setTimeout(function(){
-  inlineCss(html, {url: ' '})
-    .then(function(html){
-    fs.writeFile('./out/out.html', html, (error, data) => {
+function readFiles() {
+  return new Promise(function (resolve, reject) {
+    fs.readFile('./origin/origin.html', 'utf8', (error, data) => {
       if(error) throw error;
       console.log("success");
+      html = data;
+      resolve(html);
     });
   });
-}, 1000)
+}
+
+// after read files
+readFiles().then(function(resolveData) {
+  // writeFile
+  inlineCss(resolveData, {url: ' '})
+    .then(function(resolveData){
+      fs.writeFile('./out/out.html', resolveData, (error, data) => {
+        if(error) throw error;
+        console.log("success");
+    });
+  });
+});
