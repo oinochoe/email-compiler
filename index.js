de6bde8292bed4b871d27354a115cd8f24f64b52
oinochoe <oinochoe@naver.com>
2020-03-27 20:04:02 +0900

    format

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ const fs = require('fs');
 const inlineCss = require('inline-css');
 const origin = 'origin';
 const out = 'out';
-const originSrc = fs.readdirSync(origin).filter(file => /\.html/.test(file)).map(files => `${origin}/${files}`);
-const outSrc = fs.readdirSync(out).filter(file => /\.html/.test(file)).map(files => `${out}/${files}`);
-let html = "";
+const originSrc = fs
+  .readdirSync(origin)
+  .filter(file => /\.html/.test(file))
+  .map(files => `${origin}/${files}`);
+const outSrc = fs
+  .readdirSync(out)
+  .filter(file => /\.html/.test(file))
+  .map(files => `${out}/${files}`);
+let html = '';
 
 for (let index = 0; index < originSrc.length; index++) {
   // after read files
   readFiles().then(function(resolveData) {
     // writeFile
-    inlineCss(resolveData, {url: ' '})
-      .then(function(resolveData){
-        fs.writeFile(outSrc[index], resolveData, (error, data) => {
-          if(error) throw error;
-          console.log("생성 완료");
+    inlineCss(resolveData, { url: ' ' }).then(function(resolveData) {
+      fs.writeFile(outSrc[index], resolveData, (error, data) => {
+        if (error) throw error;
+        console.log('생성 완료');
       });
     });
   });
 
   function readFiles() {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
       fs.readFile(originSrc[index], 'utf8', (error, data) => {
-        if(error) throw error;
-        console.log("읽기 완료");
+        if (error) throw error;
+        console.log('읽기 완료');
         html = data;
         resolve(html);
       });
     });
-  }  
+  }
 }
-
